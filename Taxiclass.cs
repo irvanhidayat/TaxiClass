e956502843d5b65d474387d10c3c0a0cd5af053d
irvanhidayat <101778631+irvanhidayat@users.noreply.github.com>
2022-03-26 18:06:42 +0700

    Add files via upload

---
+++ Taxiclass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    class Taxiclass
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+        //Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName:{0}", DriverName);
+            Console.WriteLine("OnDuty:{0}", OnDuty);
+            Console.WriteLine("NumPassenger:{0}", NumPassenger);
+
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+           
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", DriverName);
+        }
+    }
+}
